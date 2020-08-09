1bbf385c2c459a2781b79450b584d2da0b186851
LucasMull <muller.lucas@Hotmail.com>
2020-08-09 17:46:53 -0300

    feat: add number to json format conversion in stringify.c

--- src/stringify.c
+++ src/stringify.c
@@ -3,51 +3,162 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <assert.h>
 
+#define DOUBLE_LEN 21
+
 static void
-cat_update(char *dest, char *src, int *i){
-  while (*src)
-    dest[(*i)++] = *src++;
+cat_update(CJSON_data *data, char *buffer, int *i)
+{
+  while (*data)
+    buffer[(*i)++] = *data++;
 }
 
 static void
-stringify_key(char *buffer, char *key, int *i)
+stringify_data(CJSON_data *data, char *buffer, int *i)
 {
-  if(!key)
+  buffer[(*i)++] = '\"';
+  cat_update(data,buffer,i);
+  buffer[(*i)++] = '\"';
+}
 
-    return; //return if no key found or is array key
-  buffer[(*i)++] = '\"';
-  cat_update(buffer,key,i);
-  buffer[(*i)++] = '\"';
-  buffer[(*i)++] = ':';
+static void
+rtrim(CJSON_data *eval_data, int c)
+{
+  CJSON_data *end=eval_data+(strlen(eval_data)-1);
+  while (*end == c)
+    *end-- = '\0';
+}
+
+static void
+ltrim(CJSON_data *eval_data, int c)
+{
+  while (*eval_data == c){
+    int i=0;
+    while (eval_data[i] != '\0'){
+      eval_data[i] = eval_data[i+1];
+      ++i;
+    }
+  }
+}
+
+static void
+remove_char(CJSON_data *eval_data, int c)
+{
+  for (int i=0; eval_data[i] != '\0'; ++i){
+    if (eval_data[i] == c){
+      while (eval_data[i] != '\0'){
+        eval_data[i] = eval_data[i+1];
+        ++i;
+      }
+      return;
+    }
+  }
+}
+
+static void
+count_decimals(CJSON_data *temp_data, int *lcount, int *rcount)
+{
+  int i=0;
+  int rdecimals=0; //negative decimals
+  while (temp_data[i++] == '0'){
+    if (temp_data[i] == '.'){
+      ++i; //skips '.'
+      continue; //skips rdecimals decrement
+    }
+    --rdecimals;
+  }
+
+  i=0;
+  int ldecimals=0; //positive decimals
+  while (temp_data[i]){
+    if (temp_data[i] == '.')
+      break;
+    ++i;
+    ++ldecimals;
+  }
+
+  *lcount = ldecimals;
+  *rcount = rdecimals;
+}
+
+static CJSON_data*
+atoexp(CJSON_data *eval_data, int decimals)
+{
+  assert(decimals);
+
+  remove_char(eval_data,'.');
+  ltrim(eval_data,'0');
+
+  CJSON_data new_data[DOUBLE_LEN];
+
+  *new_data = *eval_data;
+  *(new_data+1) = '.';
+  strncpy(new_data+2,eval_data+1,DOUBLE_LEN-3);
+  sprintf(new_data,"%se%d",new_data,decimals);
+
+  strcpy(eval_data,new_data);
+
+  return eval_data;
+}
+
+void
+eval_numdata(CJSON_data *eval_data)
+{
+  int ldecimals=0, rdecimals=0;
+
+  rtrim(eval_data,'0');
+  count_decimals(eval_data, &ldecimals, &rdecimals);
+
+  /* CONVERT TO SCIENTIFIC NOTATION*/
+  if (ldecimals >= 18)
+    atoexp(eval_data,ldecimals);
+  if ((rdecimals+ldecimals) < 0)
+    atoexp(eval_data,rdecimals-ldecimals);
+}
+
+static void
+stringify_number(double number, char *buffer, int *i)
+{
+  if (number < 0.0){ //ABSOLUTE VALUE FOR EASIER HANDLING
+    number *= -1; //TURN NEGATIVE NUMBER POSITIVE
+    buffer[(*i)++] = '-'; //SAVE MINUS SIGN IN BUFFER
+  }
+
+  CJSON_data eval_data[DOUBLE_LEN]={'\0'};
+  snprintf(eval_data,DOUBLE_LEN-1,"%f",number);
+
+  eval_numdata(eval_data);
+
+  cat_update(eval_data,buffer,i);
 }
 
 static void
 recursive_print(CJSON_item_t *item, CJSON_types_t dtype, char *buffer, int *i)
 {
   if (item->dtype & dtype){
-    if (!(item->parent->dtype & JsonArray))
-      stringify_key(buffer,item->key,i);
+    if ((item->key) && !(item->parent->dtype & JsonArray)){
+      stringify_data(item->key,buffer,i);
+      buffer[(*i)++] = ':';
+    }
 
     switch (item->dtype){
       case JsonNull:
-        cat_update(buffer,"null",i);
+        cat_update("null",buffer,i);
         break;
       case JsonBoolean:
         if (item->value.boolean){
-          cat_update(buffer,"true",i);
+          cat_update("true",buffer,i);
           break;
         }
-        cat_update(buffer,"false",i);
+        cat_update("false",buffer,i);
         break;
       case JsonNumber:
-        buffer[(*i)++] = '0';
+        stringify_number(item->value.number,buffer,i);
         break;
       case JsonString:
-        buffer[(*i)++] = '\"';
-        cat_update(buffer,item->value.string,i);
-        buffer[(*i)++] = '\"';
+        stringify_data(item->value.string,buffer,i);
         break;
       case JsonObject:
         buffer[(*i)++] = '{';
