8dcd890450162e3a7a76fe4c3e88e5623a961d82
LucasMull <muller.lucas@Hotmail.com>
2020-08-12 13:31:54 -0300

    feat: stringify formats number to json standard

--- src/stringify.c
+++ src/stringify.c
@@ -6,7 +6,7 @@
 #include <ctype.h>
 #include <assert.h>
 
-#define DOUBLE_LEN 21
+#define DOUBLE_LEN 24
 
 static void
 cat_update(CJSON_data *data, char *buffer, int *i) {
@@ -22,14 +22,50 @@ stringify_data(CJSON_data *data, char *buffer, int *i)
   buffer[(*i)++] = '\"';
 }
 
+static void 
+format_number(CJSON_data new_data[], double number)
+{
+  int decimal=0, sign=0;
+  CJSON_data *temp_data=fcvt(number,DOUBLE_LEN-1,&decimal,&sign);
+
+  //check if value is integer
+  if (number <= LLONG_MIN || number >= LLONG_MAX || number == (long long)number){
+    sprintf(new_data,"%.lf",number); //convert integer to string
+    return;
+  }
+
+  int i=0;
+  if (sign < 0)
+    new_data[i++] = '-';
+
+  if ((decimal < -7) || (decimal > 17)){ //print scientific notation 
+    sprintf(new_data+i,"%c.%.7se%d",*temp_data,temp_data+1,decimal-1);
+    return;
+  }
+
+  char format[100];
+  if (decimal > 0){
+    sprintf(format,"%%.%ds.%%.7s",decimal);
+    sprintf(new_data+i,format,temp_data,temp_data+decimal);
+    return;
+  }
+
+  if (decimal < 0){
+    sprintf(format,"0.%0*d%%.7s",abs(decimal),0);
+    sprintf(new_data+i,format,temp_data);
+    return;
+  }
+
+  sprintf(format,"0.%%.7s");
+  sprintf(new_data+i,format,temp_data);
+}
+
 static void
 stringify_number(double number, char *buffer, int *i)
 {
   CJSON_data new_data[DOUBLE_LEN]={'\0'};
-  if (number <= LLONG_MIN || number >= LLONG_MAX || number == (long long)number) //if value is integer
-    sprintf(new_data,"%.lf",number); //convert integer to string
-  else //value is a fraction
-    gcvt(number,6,new_data); //convert fraction to string
+  format_number(new_data, number);
+
   cat_update(new_data,buffer,i); //store value in buffer
 }
 
@@ -94,7 +130,7 @@ stringify_json(CJSON_t *cjson, CJSON_types_t dtype)
   assert(cjson);
   assert(cjson->item->n);
 
-  char *buffer=calloc(1,cjson->memsize);
+  char *buffer=calloc(1,cjson->memsize+200);
 
   int i=0;
   recursive_print(cjson->item, dtype, buffer, &i);
