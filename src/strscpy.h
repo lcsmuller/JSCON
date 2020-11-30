f88587fe7fefcdfd4b085c766d52380c3ee2c925
LucasMull <muller.lucas@Hotmail.com>
2020-11-30 17:17:28 -0300

    fix: recover strscpy files

--- src/strscpy.h
+++ src/strscpy.h
@@ -20,11 +20,38 @@
  * SOFTWARE.
  */
 
-#ifndef JSCON_STRSCPY_H_
-#define JSCON_STRSCPY_H_
+#ifndef STRSCPY_H_
+#define STRSCPY_H_
 
-#include <libjscon.h>
-#include "jscon-common.h"
+#include <stddef.h>
+#include <inttypes.h>
+#include <stdint.h>
+#include <limits.h>
+
+#if SIZE_MAX == UINT_MAX
+typedef int ssize_t;
+#define SSIZE_MIN  INT_MIN
+#define SSIZE_MAX  INT_MAX
+#elif SIZE_MAX == ULONG_MAX
+typedef long ssize_t;
+#define SSIZE_MIN  LONG_MIN
+#define SSIZE_MAX  LONG_MAX
+#elif SIZE_MAX == ULLONG_MAX
+typedef long long ssize_t;
+#define SSIZE_MIN  LLONG_MIN
+#define SSIZE_MAX  LLONG_MAX
+#elif SIZE_MAX == USHRT_MAX
+typedef short ssize_t;
+#define SSIZE_MIN  SHRT_MIN
+#define SSIZE_MAX  SHRT_MAX
+#elif SIZE_MAX == UINTMAX_MAX
+typedef uintmax_t ssize_t;
+#define SSIZE_MIN  INTMAX_MIN
+#define SSIZE_MAX  INTMAX_MAX
+#else
+#error platform has exotic SIZE_MAX
+#endif
+
 
 ssize_t strscpy(char *dest, const char *src, size_t n);
 
